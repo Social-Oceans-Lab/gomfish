56b573503256cbdd6f994f9224fe8b7457316b6b
TheresaBurnham <theresa.burnham@maine.edu>
2023-03-29 18:06:40 -0700

    update description & add maine lobster by county

---
+++ data-raw/me_lob_county.R
@@ -0,0 +1,28 @@
+##### Purpose #####
+
+
+##### Load Packages #####
+library(tidyverse)
+library(readxl)
+library(usethis)
+library(janitor)
+
+##### Import Raw Data #####
+#1964-2022
+#Source: https://www.maine.gov/dmr/sites/maine.gov.dmr/files/inline-files/lobster.county.pdf
+#Adobe Acrobat Online used to convert PDF to xlsx (https://www.adobe.com/acrobat/online/pdf-to-excel.html)
+
+raw<-
+  read_xlsx("data-raw/me_lob_county.xlsx",
+                     skip=1,
+                     col_names=TRUE)
+
+me_lob_county<-
+  raw%>%
+  select(-4)%>%
+  clean_names()%>%
+  mutate_if(is.character,str_to_lower)%>%
+  filter(county!="total")
+
+write_csv(me_lob_county,"data-raw/me_lob_county.csv")
+usethis::use_data(me_lob_county,overwrite=TRUE)
