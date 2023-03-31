2d29d48a650729dec7cc1b39c1d9b204876633a0
TheresaBurnham <theresa.burnham@maine.edu>
2023-03-31 08:51:18 -0700

    housing affordability descriptions

---
+++ R/me_housing_affordability.R
@@ -0,0 +1,15 @@
+#' @title Maine: Annual Housing Affordability by Towns and Cities
+#'
+#' @description A dataset with annual housing affordability metrics from Maine Housing from 2000-2022
+#'
+#' @format A data frame with 443 rows and 4 variables:
+#' \describe{
+#'   \item{name}{Landing year.}
+#'   \item{year}{Maine county.}
+#'   \item{affordability}{Total pounds of lobster landed by the commercial fishery in a given year and county}
+#'   \item{affordable_homes_sold}{Total value of lobster landed by the commercial fishery in a given year and county}
+#'   \item{home_price_affordable_to_median_income}
+#'   \item{}
+#' }
+#' @source <https://github.com/Social-Oceans-Lab/gomfish>
+"me_lob_county"
--- data-raw/me_housing_affordability.R
+++ data-raw/me_housing_affordability.R
@@ -20,3 +20,27 @@ me_housing_affordability<-
   mutate_if(is.character,str_to_lower)
 
 usethis::use_data(me_housing_affordability,overwrite=TRUE)
+
+
+##### Create roxygen2 description #####
+create_roxygen_file <- function(df, file_path) {
+  # Get column names of the data frame
+  column_names <- colnames(df)
+
+  # Create a character vector with the roxygen2 format for each column
+  column_roxygen <- paste0("#' @param ", column_names, " Description of ", column_names, ".")
+
+  # Combine the column roxygen into a single character string
+  file_contents <- paste0(c("#' Title: ", deparse(substitute(df)),
+                            "\n#' Description: A dataset containing ", nrow(df), " rows and ",
+                            ncol(df), " columns.",
+                            "\n#' @param df A data frame.",
+                            "\n", column_roxygen), collapse = "\n")
+
+  # Write the file to disk
+  write(file_contents, file = file_path)
+}
+
+# Example usage
+my_data <- data.frame(x = 1:10, y = 11:20)
+create_roxygen_file(my_data, "my_data.R")
