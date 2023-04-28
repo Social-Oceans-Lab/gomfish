cd829d1c1c45a1c5f7781ed2c92a93452178d323
TheresaBurnham <theresa.burnham@maine.edu>
2023-04-28 14:30:20 -0700

    trying agin with smaller file

--- data-raw/ind_lic_portfolio.R
+++ data-raw/ind_lic_portfolio.R
@@ -1,8 +1,11 @@
 ##### Purpose #####
 #To import and document the individual license portfolios of fishermen in Maine from 2016-2021
 
+##### Load Packages #####
+library(tidyverse)
+
 ##### Import Raw Data #####
-ind_lic_portfolio<-read_csv("../social_indicators/Secondary_Data/licenses/tidy_data/ind_lic_portfolio.csv")
+ind_lic_portfolio<-read_csv("../social_indicators/Secondary_Data/licenses/tidy_data/ind_license_portfolio_1990_2022.csv")
 
 ##### Add to data folder #####
 usethis::use_data(ind_lic_portfolio, overwrite = TRUE)
