c3dbcacec770d553f09bf5d94dc4d50c5554fea5
TheresaBurnham <theresa.burnham@maine.edu>
2023-07-14 17:04:53 -0700

    diwf boat regs

---
+++ R/me_diwf_boat_regs.R
@@ -0,0 +1,9 @@
+#' @title Annual State Boat Registrations (Maine Department of Inland Fisheries & Wildlife)
+#' @description A dataset with annual DIWF boat registration information in Maine from 2003-2021
+#' @format A data frame with 19 rows and 2 variables:
+#' \describe{
+#'   \item{year}{DIWF boat registration year}
+#'   \item{total_diwf_boat_regs}{Number of boats registered with DIWF}
+#'}
+#' @source <https://github.com/Social-Oceans-Lab/gomfish>
+"me_diwf_boat_regs"
