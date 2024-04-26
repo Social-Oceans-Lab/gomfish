9fd5985730d51640ca25bb63d8fde3b4b4649e1a
TheresaBurnham <theresa.burnham@maine.edu>
2024-04-26 14:23:13 +0200

    adding 2023 licenses to individual license portfolio

--- data-raw/ind_lic_portfolio.R
+++ data-raw/ind_lic_portfolio.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 ##### Import Raw Data #####
 ind_lic_portfolio<-
-  read_csv(file=gzfile("../solab_data/me_licenses/tidy_data/ind_license_portfolio_1990_2022.csv.gz"))
+  read_csv(file=gzfile("../solab_data/me_licenses/tidy_data/ind_license_portfolio_1990_2023.csv.gz"))
 
 ##### Add to data folder #####
 usethis::use_data(ind_lic_portfolio, overwrite = TRUE)
