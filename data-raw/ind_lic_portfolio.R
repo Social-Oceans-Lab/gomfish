1c00aec40328dd20e0e51f3633e6a5709826ba49
TheresaBurnham <theresa.burnham@maine.edu>
2026-02-23 09:40:24 -0800

    updating individual license portfolio to 2025

--- data-raw/ind_lic_portfolio.R
+++ data-raw/ind_lic_portfolio.R
@@ -1,12 +1,12 @@
 ##### Purpose #####
-#To import and document the individual license portfolios of fishermen in Maine from 2008-2022
+#To import and document the individual license portfolios of fishermen in Maine from 1990-2025
 
 ##### Load Packages #####
 library(tidyverse)
 
 ##### Import Raw Data #####
 ind_lic_portfolio<-
-  read_csv(file=gzfile("../solab_data/me_licenses/tidy_data/ind_license_portfolio_1990_2023.csv.gz"))
+  read_csv(file=gzfile("../solab_data/me_licenses/tidy_data/ind_license_portfolio_1990_2025.csv.gz"))
 
 ##### Add to data folder #####
 usethis::use_data(ind_lic_portfolio, overwrite = TRUE)
